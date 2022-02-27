e7c3158ffb1df272c79accc6c35f9f2ef953afa3
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2022-02-27 17:26:45 -0500

    commit tree 8048c3e232ff0d086026101a49296d12f6674874

---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+
+INSERT into bears (name, age, sex, color, temperament, alive) 
+VALUES 
+("Mr. Chocolate", 13, "M", "Black", "Malicious", True),
+
+("Rowdy", 2, "F", "Blue", "Happy", False),
+
+("Tabitha", 8, "F", "Gold", "Joyous", True),
+
+("Sergeant Brown", 7, "M", "Brown", "Mean", False),
+
+("Melissa", 8, "F", "Pink", "Lively", True),
+
+("Grinch", 30, "M", "Green", "Grouchy", True),
+
+("Wendy", 8, "F", "Yellow", "Gregarious", False),
+
+(NULL, 1, "F", "Yellow", "Gregarious", False);
